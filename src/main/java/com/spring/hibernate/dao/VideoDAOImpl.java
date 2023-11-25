f77eea376eb1cceb1ee860e1b7164ed2e787662c
Ben Hsieh <bqh5026@gmail.com>
2023-11-24 23:34:35 -0600

    Update VideoDAOImpl

--- src/main/java/com/spring/hibernate/dao/VideoDAOImpl.java
+++ src/main/java/com/spring/hibernate/dao/VideoDAOImpl.java
@@ -1,5 +1,36 @@
 package com.spring.hibernate.dao;
 
-public class VideoDAOImpl {
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.spring.hibernate.model.Video;
+
+@Repository
+public class VideoDAOImpl implements VideoDAO{
+
+	@Override
+	public List<Video> get() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Video get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Video video) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
