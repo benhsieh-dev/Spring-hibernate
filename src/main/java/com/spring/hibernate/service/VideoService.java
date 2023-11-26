cb7abfee712daf70ae15bd3d9c9faf767cf25d22
Ben Hsieh <bqh5026@gmail.com>
2023-11-25 23:58:16 -0600

    Update video service

--- src/main/java/com/spring/hibernate/service/VideoService.java
+++ src/main/java/com/spring/hibernate/service/VideoService.java
@@ -1,5 +1,17 @@
 package com.spring.hibernate.service;
 
+import java.util.List;
+
+import com.spring.hibernate.model.Video;
+
 public interface VideoService {
+	
+	List<Video> get();
+	
+	Video get(int id);
+	
+	void save(Video video);
+	
+	void delete(int id); 
 
 }
