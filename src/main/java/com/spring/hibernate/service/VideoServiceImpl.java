3aefef9be784c008133c36f3098cc9e6e718d5f0
Ben Hsieh <bqh5026@gmail.com>
2023-11-26 23:16:37 -0600

    Update video service impl

--- src/main/java/com/spring/hibernate/service/VideoServiceImpl.java
+++ src/main/java/com/spring/hibernate/service/VideoServiceImpl.java
@@ -1,5 +1,33 @@
 package com.spring.hibernate.service;
 
+import java.util.List;
+
+import com.spring.hibernate.model.Video;
+
 public class VideoServiceImpl implements VideoService {
+
+	@Override
+	public List<Video> get() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Video get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Video video) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
