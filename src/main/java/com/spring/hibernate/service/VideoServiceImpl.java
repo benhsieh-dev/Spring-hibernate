919551852062f3f369729ee1b5e6ad271be98359
Ben Hsieh <bqh5026@gmail.com>
2023-11-27 23:37:45 -0600

    Update video service impl

--- src/main/java/com/spring/hibernate/service/VideoServiceImpl.java
+++ src/main/java/com/spring/hibernate/service/VideoServiceImpl.java
@@ -2,28 +2,36 @@ package com.spring.hibernate.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.spring.hibernate.model.Video;
 
+@Service
 public class VideoServiceImpl implements VideoService {
-
+	
+	@Transactional
 	@Override
 	public List<Video> get() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional
 	@Override
 	public Video get(int id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional
 	@Override
 	public void save(Video video) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Transactional
 	@Override
 	public void delete(int id) {
 		// TODO Auto-generated method stub
