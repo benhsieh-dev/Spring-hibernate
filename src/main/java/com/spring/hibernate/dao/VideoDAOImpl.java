081f81f6c0a501937e17964b0a37678385ab579c
Ben Hsieh <bqh5026@gmail.com>
2023-11-28 22:43:46 -0600

    Update video dao impl

--- src/main/java/com/spring/hibernate/dao/VideoDAOImpl.java
+++ src/main/java/com/spring/hibernate/dao/VideoDAOImpl.java
@@ -2,17 +2,28 @@ package com.spring.hibernate.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.spring.hibernate.model.Video;
 
 @Repository
 public class VideoDAOImpl implements VideoDAO{
+	
+	@Autowired
+	private EntityManager entityManager; 
 
 	@Override
 	public List<Video> get() {
-		// TODO Auto-generated method stub
-		return null;
+		Session currentSession = entityManager.unwrap(Session.class);
+		Query<Video> query = currentSession.createQuery("From Video", Video.class);
+		query.getResultList();
+		List<Video> list = query.getResultList();
+		return list; 
 	}
 
 	@Override
