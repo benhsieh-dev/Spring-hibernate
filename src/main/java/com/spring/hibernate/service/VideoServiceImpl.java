33f2da632f11f92b854fc48eb117993b1d4301e3
Ben Hsieh <bqh5026@gmail.com>
2023-11-29 22:55:00 -0600

    Update video service impl

--- src/main/java/com/spring/hibernate/service/VideoServiceImpl.java
+++ src/main/java/com/spring/hibernate/service/VideoServiceImpl.java
@@ -2,13 +2,18 @@ package com.spring.hibernate.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.spring.hibernate.dao.VideoDAO;
 import com.spring.hibernate.model.Video;
 
 @Service
 public class VideoServiceImpl implements VideoService {
+	
+	@Autowired
+	private VideoDAO videoDAO;
 	
 	@Transactional
 	@Override
