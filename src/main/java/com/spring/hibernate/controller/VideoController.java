e75d5588083d5059f382b5089dd01f6c588da3b2
Ben Hsieh <bqh5026@gmail.com>
2023-12-03 23:03:05 -0600

    Update app

--- src/main/java/com/spring/hibernate/controller/VideoController.java
+++ src/main/java/com/spring/hibernate/controller/VideoController.java
@@ -3,6 +3,7 @@ package com.spring.hibernate.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class VideoController {
 	@Autowired
 	private VideoService videoService;
 	
-	@RequestMapping("/video")
+	@GetMapping("/video")
 	public List<Video> get() {
 		return videoService.get();
 	}
