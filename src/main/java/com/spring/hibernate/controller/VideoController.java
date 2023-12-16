5b75715a8368290340654e11d3128f4dde36b31b
Ben Hsieh <bqh5026@gmail.com>
2023-12-15 23:18:48 -0600

    Update video controller

--- src/main/java/com/spring/hibernate/controller/VideoController.java
+++ src/main/java/com/spring/hibernate/controller/VideoController.java
@@ -3,6 +3,7 @@ package com.spring.hibernate.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,4 +36,11 @@ public class VideoController {
 	public Video get(@PathVariable int id) {
 		return videoService.get(id);
 	}
+	
+	@DeleteMapping("/video/{id}")
+	public String delete(@PathVariable int id) {
+		videoService.delete(id);
+		return "Video has been deleted with id: " + id;
+	}
+	
 }
